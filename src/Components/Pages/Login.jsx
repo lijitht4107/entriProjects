bfc5dd57d2bed6cb5fa4e1bda7d9f1f85bd699ae
lijitht4107 <lijiththazhathethil@gmail.com>
2023-11-13 22:01:20 +0530

    halloworld

---
+++ src/Components/Pages/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+import LoginArea from '../Commen/LoginArea'
+
+function Login() {
+  return (
+   <>
+   <div >
+
+   <LoginArea/>
+
+   </div>
+   </>
+  )
+}
+
+export default Login
